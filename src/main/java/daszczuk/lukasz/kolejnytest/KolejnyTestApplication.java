a3f78054b6ea871b93b2f5918e4bea9593125405
Łukasz Daszczuk <lukasz3922@gmail.com>
2018-09-24 18:56:24 +0200

    first commit

---
+++ src/main/java/daszczuk/lukasz/kolejnytest/KolejnyTestApplication.java
@@ -0,0 +1,12 @@
+package daszczuk.lukasz.kolejnytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolejnyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolejnyTestApplication.class, args);
+	}
+}
---
+++ src/test/java/daszczuk/lukasz/kolejnytest/KolejnyTestApplicationTests.java
@@ -0,0 +1,16 @@
+package daszczuk.lukasz.kolejnytest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KolejnyTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
